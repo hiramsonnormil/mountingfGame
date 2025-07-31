9dd009809ae8f748be49cda0c7333a7f690b3495
bia-gonzalez <biaareserva027@gmail.com>
2025-07-31 15:50:14 -0300

    creat intity class

---
+++ code/entity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.Attribute1 = None
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def Operation1(self, ):
+        pass
+
+    def move(self, ):
+        pass
